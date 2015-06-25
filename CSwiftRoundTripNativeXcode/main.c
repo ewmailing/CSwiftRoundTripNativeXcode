bbda39e9f55dcf10dd968b288b7a91ce30d2d401
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-06-25 06:05:26 -0700

    Initial Commit

---
+++ CSwiftRoundTripNativeXcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSwiftRoundTripNativeXcode
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
